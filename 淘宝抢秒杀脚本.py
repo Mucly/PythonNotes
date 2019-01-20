6826418556a92d825f80836cf57051b5f265a63d
Mucly <18329023996@sina.cn>
2019-01-20 12:46:15 +0800

    淘宝秒杀脚本已失效

--- 淘宝抢秒杀脚本.py
+++ 淘宝抢秒杀脚本.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 2018/09/05
-# 淘宝秒杀脚本，扫码登录版
+# （已失效）淘宝秒杀脚本，扫码登录版
 from selenium import webdriver
 import datetime
 import time
 
 
-def login():
+def login(browser):
     # 打开淘宝登录页，并进行扫码登录
     browser.get("https://www.taobao.com")
     time.sleep(3)
     if browser.find_element_by_link_text("亲，请登录"):
         browser.find_element_by_link_text("亲，请登录").click()
-        print("请在15秒内完成扫码")
-        time.sleep(15)
+        print("请在8秒内完成扫码")
+        time.sleep(8)
         browser.get("https://cart.taobao.com/cart.htm")
     time.sleep(3)
 
@@ -22,22 +22,18 @@ def login():
     print('login success:', now.strftime('%Y-%m-%d %H:%M:%S'))
 
 
-def buy(times, choose):
-    # 点击购物车里全选按钮
-    if choose == 2:
-        print("请手动勾选需要购买的商品")
+def buy(browser, times):
     while True:
         now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
         # 对比时间，时间到的话就点击结算
         if now > times:
-            if choose == 1:
-                while True:
-                    try:
-                        if browser.find_element_by_id("J_SelectAll1"):
-                            browser.find_element_by_id("J_SelectAll1").click()
-                            break
-                    except:
-                        print("找不到全选按钮")
+            while True:
+                try:
+                    if browser.find_element_by_id("J_SelectAll1"):
+                        browser.find_element_by_id("J_SelectAll1").click()
+                        break
+                except:
+                    print("找不到全选按钮")
             # 点击结算按钮
             try:
                 if browser.find_element_by_id("J_Go"):
@@ -56,10 +52,11 @@ def buy(times, choose):
             time.sleep(0.01)
 
 if __name__ == "__main__":
-    times = input("请输入抢购时间，格式如(2018-09-06 11:20:00.000000):")
+    cur_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+    times = input(f"请输入抢购时间，格式如({cur_time}):\n")
     # 时间格式："2018-09-06 11:20:00.000000"
-    browser = webdriver.Chrome()
-    browser.maximize_window()
-    login()
-    choose = input("到时间自动勾选购物车请输入“1”，否则输入“2”：")
-    buy(times, choose)
+    chrome_browser = webdriver.Chrome()
+    chrome_browser.maximize_window()
+
+    login(chrome_browser)
+    buy(chrome_browser, times)
