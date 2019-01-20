e03ea901c57f924eb9f8f489ebafac5a13180bcc
Mucly <18329023996@sina.cn>
2019-01-20 15:45:48 +0800

    提交修改

--- batch_modify_file.py
+++ batch_modify_file.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 2019/1/20
+# 将basepath路径下的txt文本后缀改为rgb
+
 import os
 
-# 将basepath路径下的txt文本后缀改为rgb
 basepath = r"D:\tmp"
 res_suffix = ".txt"
 dst_suffix = ".rgb"
 for file in os.listdir(basepath):
-    head, tail = os.path.splitext(file)
+    head, tail = os.path.splitext(file)  # 分离文件名和文件后缀
     if tail == res_suffix:
         new_file = head + dst_suffix
         os.rename(os.path.join(basepath, file),
