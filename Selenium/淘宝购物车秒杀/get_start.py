b7645b6722887c24f8e90ada1a4b9fa31e56718c
Mucly <18329023996@sina.cn>
2019-01-20 23:03:41 +0800

    删除无用文件

--- Selenium/淘宝购物车秒杀/get_start.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-
-driver = webdriver.Chrome()  # 创建一个Chrome浏览器对象
-driver.get("https://www.taobao.com")
-driver.close()
