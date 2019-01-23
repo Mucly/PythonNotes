f71dbb11a236ac9cda9eaa2f5158cc77484f37ee
Mucly <18329023996@sina.cn>
2019-01-23 09:41:25 +0800

    本地电脑提交

--- batch_modify_file.py
+++ batch_modify_file.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 2019/1/20
-# 将basepath路径下的txt文本后缀改为rgb
 
 import os
+import fnmatch
 
-basepath = r"D:\tmp"
-res_suffix = ".txt"
-dst_suffix = ".rgb"
-for file in os.listdir(basepath):
-    head, tail = os.path.splitext(file)  # 分离文件名和文件后缀
-    if tail == res_suffix:
-        new_file = head + dst_suffix
-        os.rename(os.path.join(basepath, file),
-                  os.path.join(basepath, new_file))
+
+def changeSuffix(path, res_suffix, dst_suffix):
+    '''
+        将basepath路径下，所有后缀为res_suffix的文件，后缀改为dst_suffix
+        后缀即文件的拓展名，即文件类型
+    '''
+    for file in os.listdir(path):
+        head, tail = os.path.splitext(file)  # 分离文件名和文件后缀
+        if tail == res_suffix:
+            new_file = head + dst_suffix
+            os.rename(os.path.join(path, file),
+                      os.path.join(path, new_file))
+
+if __name__ == '__main__':
+    pass
+    basepath = os.path.realpath('.')
+    # changeSuffix(basepath, '.aaa', '.txt') # 将文件后缀为.aaa的文件，后缀改为.txt
